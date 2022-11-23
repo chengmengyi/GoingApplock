b5798d35251c5c5de13ff80038ed5cf3acbfb571
loadee <downloader@der.com>
2022-11-23 14:47:03 +0800

    1.0.0

--- app/src/main/java/com/demo/goingapplock/conf/GoingConf.kt
+++ app/src/main/java/com/demo/goingapplock/conf/GoingConf.kt
@@ -1,10 +1,13 @@
 package com.demo.goingapplock.conf
 
 object GoingConf {
-    const val look="To use the application lock function, you need to obtain the permission of \"View Application Usage\""
-    const val resetPwd="Do you want to reset your password?\n" + "if you reset the password, all currently encrypted data will also be deleted!"
-    const val overlay="To use the application locking function, you need to obtain the permission of \"Floating Window\""
+    const val look =
+        "To use the application lock function, you need to obtain the permission of \"View Application Usage\""
+    const val resetPwd =
+        "Do you want to reset your password?\n" + "if you reset the password, all currently encrypted data will also be deleted!"
+    const val overlay =
+        "To use the application locking function, you need to obtain the permission of \"Floating Window\""
 
-    const val URL=""
-    const val EMAIL=""
+    const val URL = "https://sites.google.com/view/goingapplockwifianalyzer/home"
+    const val EMAIL = "whiteblack20210706@gmail.com"
 }
