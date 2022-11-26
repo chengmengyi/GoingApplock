a5ea25bfff7dac46761635d9988aea4eff6e1935
loadee <downloader@der.com>
2022-11-26 16:51:31 +0800

    add release configure

--- app/src/main/java/com/demo/goingapplock/ad/LocalConfigure.kt
+++ app/src/main/java/com/demo/goingapplock/ad/LocalConfigure.kt
@@ -2,65 +2,48 @@ package com.demo.goingapplock.ad
 
 const val LOCAL_ADC_LIST_GOING_STR="""
 {
-  "gawsh":50,
-  "gawck":15,
-  "gaw_open": [
-    {
-      "gaw_tp": "o",
-      "gaw_wf": 3,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/3419835294xx"
-    }
-  ],
-  "gaw_home_nt": [
-    {
-      "gaw_tp": "n",
-      "gaw_wf": 1,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/2247696110xx"
-    }
-  ],
-  "gaw_wifien_it": [
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 2,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/8691691433xx"
-    },
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 1,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/1033173712xx"
-    }
-  ],
-  "gaw_pass_it": [
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 2,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/8691691433xx"
-    },
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 1,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/1033173712xx"
-    }
-  ],
-  "gaw_return_it": [
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 2,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/8691691433xx"
-    },
-    {
-      "gaw_tp": "i",
-      "gaw_wf": 1,
-      "gaw_so": "admob",
-      "gaw_id": "ca-app-pub-3940256099942544/1033173712xx"
-    }
-  ]
+    "gawsh": 50,
+    "gawck": 15,
+    "gaw_open": [
+        {
+            "gaw_tp": "o",
+            "gaw_wf": 1,
+            "gaw_so": "admob",
+            "gaw_id": "cca-app-pub-7891246192306568/2657056182"
+        }
+    ],
+    "gaw_home_nt": [
+        {
+            "gaw_tp": "n",
+            "gaw_wf": 1,
+            "gaw_so": "admob",
+            "gaw_id": "ca-app-pub-7891246192306568/1343974514"
+        }
+    ],
+    "gaw_wifien_it": [
+        {
+            "gaw_tp": "i",
+            "gaw_wf": 1,
+            "gaw_so": "admob",
+            "gaw_id": "ca-app-pub-7891246192306568/2732830384"
+        }
+    ],
+    "gaw_pass_it": [
+        {
+            "gaw_tp": "i",
+            "gaw_wf": 1,
+            "gaw_so": "admob",
+            "gaw_id": "ca-app-pub-7891246192306568/1419748719"
+        }
+    ],
+    "gaw_return_it": [
+        {
+            "gaw_tp": "i",
+            "gaw_wf": 1,
+            "gaw_so": "admob",
+            "gaw_id": "ca-app-pub-7891246192306568/6720176850"
+        }
+    ]
+ 
 }
 """
